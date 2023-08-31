cadc3f5338f6bf9262503e06214b8fb9ca30f9ef
facusteckler86 <facundomsteckler@gmail.com>
2023-08-31 01:06:39 -0300

    octavo commit

--- src/app/Layout/navbar/pages/home/home.module.ts
+++ src/app/Layout/navbar/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
